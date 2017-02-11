336c906be60cced9d9226bdfc574c2cee7ef278a
Mark Grob <me@markgrob.com>
2017-02-10 22:44:31 -0500

    inital commit
    Adding in all source from my local project.

---
+++ Assets/SavannaHouse/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+
+		if(Input.GetKey(KeyCode.Keypad0)){
+			Application.LoadLevel(0);
+		}
+		if(Input.GetKey(KeyCode.Keypad1)){
+			Application.LoadLevel(1);
+		}
+		if(Input.GetKey(KeyCode.Keypad2)){
+			Application.LoadLevel(2);
+		}
+	}
+}
